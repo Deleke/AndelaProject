681591000d93abc45333024c591e621b108ed60c
Adeleke <adeleketogun@gmail.com>
2014-08-13 13:02:58 +0100

    Here we go, Our first commit

---
+++ quizz/app/views/quizzes/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @quiz, :id, :question, :ans1, :ans2, :ans3, :ans4, :correctAns, :created_at, :updated_at
---
+++ quizz/db/migrate/20140813101023_create_quizzes.rb
@@ -0,0 +1,14 @@
+class CreateQuizzes < ActiveRecord::Migration
+  def change
+    create_table :quizzes do |t|
+      t.text :question
+      t.text :ans1
+      t.text :ans2
+      t.text :ans3
+      t.text :ans4
+      t.integer :correctAns
+
+      t.timestamps
+    end
+  end
+end
